c95a46bf1670746f9693b50d932ec0e7ef31913a
Zainab Almoamen <almoamen@umich.edu>
2022-10-20 17:18:20 -0400

    added the word CORRECT

--- quraticsolver.java
+++ quraticsolver.java
@@ -10,7 +10,7 @@ public class quraticsolver {
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
 			{
-				System.out.println("The solution for the equation " + a + "x^2 + " + b +"x + " + c +" are");
+				System.out.println("The CORRECT solution for the equation " + a + "x^2 + " + b +"x + " + c +" are");
 				System.out.println((-b+Math.sqrt(b*b-4*a*c))/2*a + " and " + (-b-Math.sqrt(b*b-4*a*c))/2*a);
 			}
 	}
